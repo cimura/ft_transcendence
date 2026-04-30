b5e3b921dbab1927a3649ab7f1062cd2f84985cf
cimy <githubhoge@gmail.com>
2026-04-30 22:32:01 +0900

    テストファイルPush

---
+++ .github/workflows/test.js
@@ -0,0 +1,17 @@
+// test.js
+function calculate(a, b) {
+  // 変数名が適当
+  // 未使用の変数がある
+  const unused = 10;
+  
+  // マジックナンバー
+  if (a == 100) {
+    return "Perfect";
+  }
+
+  // 型に厳密でない比較
+  return a + b
+}
+
+// console.log の消し忘れ
+console.log(calculate(10, 20));
