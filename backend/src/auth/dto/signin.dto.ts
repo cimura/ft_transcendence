3d86d812e9411197571800426859907dea0e1e66
Takato Takino <ttakino@c4r3s15.42tokyo.jp>
2026-05-24 18:51:29 +0900

    style: format

--- backend/src/auth/dto/signin.dto.ts
+++ backend/src/auth/dto/signin.dto.ts
@@ -1,18 +1,21 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignInRequestDto {
   @ApiProperty({
     example: 'example@example.com',
-    description: 'ユーザーのメールアドレス (ユニーク)'
+    description: 'ユーザーのメールアドレス (ユニーク)',
   })
-  @IsEmail({}, { message: 'Please enter your email address in the correct format.' })
+  @IsEmail(
+    {},
+    { message: 'Please enter your email address in the correct format.' },
+  )
   @IsNotEmpty({ message: 'Email is required.' })
   email: string;
 
   @ApiProperty({
     example: 'password123',
-    description: 'サインイン用のパスワード (8文字以上)'
+    description: 'サインイン用のパスワード (8文字以上)',
   })
   @IsString()
   @IsNotEmpty({ message: 'Password is required.' })
@@ -22,7 +25,7 @@ export class SignInRequestDto {
 export class SignInResponseDto {
   @ApiProperty({
     example: 'eyJhbGciOiJIUz...',
-    description: '今後APIにアクセスするために使う鍵'
+    description: '今後APIにアクセスするために使う鍵',
   })
   accessToken: string;
 }
--- backend/src/auth/dto/signup.dto.ts
+++ backend/src/auth/dto/signup.dto.ts
@@ -1,18 +1,21 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignUpRequestDto {
   @ApiProperty({
     example: 'example@example.com',
-    description: 'ユーザーのメールアドレス (ユニーク)'
+    description: 'ユーザーのメールアドレス (ユニーク)',
   })
-  @IsEmail({}, { message: 'Please enter your email address in the correct format.' })
+  @IsEmail(
+    {},
+    { message: 'Please enter your email address in the correct format.' },
+  )
   @IsNotEmpty({ message: 'Email is required.' })
   email: string;
 
   @ApiProperty({
     example: 'password123',
-    description: 'サインイン用のパスワード (8文字以上)'
+    description: 'サインイン用のパスワード (8文字以上)',
   })
   @IsString()
   @IsNotEmpty({ message: 'Password is required.' })
@@ -23,19 +26,19 @@ export class SignUpRequestDto {
 export class SignUpResponseDto {
   @ApiProperty({
     example: '3f9b7b2a-...',
-    description: 'ユーザーID'
+    description: 'ユーザーID',
   })
   id: string;
 
   @ApiProperty({
     example: 'user@example.com',
-    description: 'メールアドレス'
+    description: 'メールアドレス',
   })
   email: string;
 
   @ApiProperty({
     example: 'eyJhbGciOiJIUz...',
-    description: '今後APIにアクセスするために使う鍵'
+    description: '今後APIにアクセスするために使う鍵',
   })
   accessToken: string;
 }
