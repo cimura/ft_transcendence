c1351fa53181fbd2cbe2bbcfe01ec338ae09f096
taka2162 <talat.talat@outlook.com>
2026-05-28 23:02:58 +0900

    chore: テストのeslint基準を緩めた

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -36,6 +36,8 @@ export default tseslint.config(
     files: ['**/*.spec.ts', '**/*.test.ts'],
     rules: {
       '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 );
