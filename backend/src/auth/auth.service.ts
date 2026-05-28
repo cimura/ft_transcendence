16a13e6b89810231d37e3f7e5edd59b16657d4c5
taka2162 <talat.talat@outlook.com>
2026-05-28 22:49:10 +0900

    fix: 不要なコメントを削除・authテストを更新

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,13 +4,11 @@ import {
   ConflictException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 import { SignUpRequestDto, SignUpResponseDto } from './dto/signup.dto';
 import { SignInRequestDto, SignInResponseDto } from './dto/signin.dto';
 import { ProfileUserDto } from './dto/profile.dto';
 import * as bcrypt from 'bcrypt';
-
-// DBが完成するまでの仮の保存場所（メモリ上の配列）
 
 @Injectable()
 export class AuthService {
@@ -33,7 +31,6 @@ export class AuthService {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(dto.password, saltRounds);
 
-    // モックDBに保存
     const user = await this.prisma.user.create({
       data: {
         email: dto.email,
