7a7dd5b2cc50fc66d3e673e4691cf189ec3ddfbb
Ruiko Seki <rseki@c3r3s5.42tokyo.jp>
2026-06-05 17:13:22 +0900

    fix: update friends request user type

--- backend/src/friends/friends.controller.ts
+++ backend/src/friends/friends.controller.ts
@@ -12,7 +12,7 @@ import {
 import { FriendsService } from './friends.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { SendFriendRequestDto } from './dto/send-friend-request.dto';
-import type { AuthenticatedRequest } from '../auth/interfaces/auth.interface';
+import type { UserRequest } from '../users/interfaces/user-request.interface';
 
 @Controller('friends')
 @UseGuards(JwtAuthGuard)
@@ -20,7 +20,7 @@ export class FriendsController {
   constructor(private friendsService: FriendsService) {}
 
   @Get()
-  friendsGet(@Request() req: AuthenticatedRequest) {
+  friendsGet(@Request() req: UserRequest) {
     const currentUserId = req.user.userId;
     return this.friendsService.getFriends(currentUserId);
   }
@@ -28,7 +28,7 @@ export class FriendsController {
   // send request
   @Post('request')
   friendsRequest(
-    @Request() req: AuthenticatedRequest,
+    @Request() req: UserRequest,
     @Body() body: SendFriendRequestDto,
   ) {
     const currentUserId = req.user.userId;
@@ -38,36 +38,27 @@ export class FriendsController {
 
   // get all request(list)
   @Get('requests')
-  friendsRequests(@Request() req: AuthenticatedRequest) {
+  friendsRequests(@Request() req: UserRequest) {
     const currentUserId = req.user.userId;
     return this.friendsService.getFriendsRequests(currentUserId);
   }
 
   @Put(':requestId/accept')
-  friendsAccept(
-    @Request() req: AuthenticatedRequest,
-    @Param('requestId') id: string,
-  ) {
+  friendsAccept(@Request() req: UserRequest, @Param('requestId') id: string) {
     const currentUserId = req.user.userId;
     const friendshipId = id;
     return this.friendsService.acceptRequest(currentUserId, friendshipId);
   }
 
   @Put(':requestId/reject')
-  friendsReject(
-    @Request() req: AuthenticatedRequest,
-    @Param('requestId') id: string,
-  ) {
+  friendsReject(@Request() req: UserRequest, @Param('requestId') id: string) {
     const currentUserId = req.user.userId;
     const friendshipId = id;
     return this.friendsService.rejectRequest(currentUserId, friendshipId);
   }
 
   @Delete(':userId')
-  friendsDelete(
-    @Request() req: AuthenticatedRequest,
-    @Param('userId') id: string,
-  ) {
+  friendsDelete(@Request() req: UserRequest, @Param('userId') id: string) {
     const currentUserId = req.user.userId;
     const friendUserId = id;
     return this.friendsService.deleteFriend(currentUserId, friendUserId);
