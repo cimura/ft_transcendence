96478e57423f65dc84d934c24ef26079b3b4604a
Takato Takino <ttakino@c5r2s1.42tokyo.jp>
2026-07-16 18:09:03 +0900

    fix(backend): Lintエラー

--- backend/src/game/game-exception.filter.ts
+++ backend/src/game/game-exception.filter.ts
@@ -1,22 +1,28 @@
 import { Catch, ArgumentsHost, Logger, ExceptionFilter } from '@nestjs/common';
 import { WsException } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { GameSocket } from './game.types';
 
 @Catch()
 export class GameExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GameExceptionFilter.name);
 
   catch(exception: unknown, host: ArgumentsHost) {
-    const client = host.switchToWs().getClient<Socket>();
+    const client = host.switchToWs().getClient<GameSocket>();
 
     let message = 'An unexpected error occurred';
 
     if (exception instanceof WsException) {
       const errorData = exception.getError();
-      message =
-        typeof errorData === 'string'
-          ? errorData
-          : (errorData as any).message || message;
+
+      if (typeof errorData === 'string') {
+        message = errorData;
+      } else if (
+        typeof errorData === 'object' &&
+        errorData !== null &&
+        'message' in errorData
+      ) {
+        message = String(errorData.message);
+      }
     } else if (exception instanceof Error) {
       message = exception.message;
     }
