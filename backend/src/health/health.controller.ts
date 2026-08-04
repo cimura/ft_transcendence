02b1e793ab1b006ba1f414e1ff7153513105f722
Takato Takino <ttakino@c5r4s2.42tokyo.jp>
2026-08-04 18:01:04 +0900

    docs(backend): health checkのSwaggerDocumentsにスキーマを追加

--- backend/src/health/health.controller.ts
+++ backend/src/health/health.controller.ts
@@ -6,7 +6,16 @@ import { ApiOperation, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 export class HealthController {
   @Get()
   @ApiOperation({ summary: 'ヘルスチェック用エンドポイント' })
-  @ApiOkResponse({ description: 'アプリケーションが起動していることを示す' })
+  @ApiOkResponse({
+    description: 'アプリケーションが起動していることを示す',
+    schema: {
+      type: 'object',
+      properties: {
+        status: { type: 'string', enum: ['ok'] },
+      },
+      required: ['status'],
+    },
+  })
   check(): { status: 'ok' } {
     return { status: 'ok' };
   }
